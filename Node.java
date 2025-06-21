1ff48d32d0c025359127fdf73d114ac10487ce31
jecasais <casais.elohym@correo.unimet.edu.ve>
2025-06-20 21:59:14 -0400

    Update Node.java

--- Node.java
+++ Node.java
@@ -8,10 +8,60 @@ package boceto.de.proyecto;
  *
  * @author elohym
  */
-class Node {
 
-    void setAttribute(String uilabel, String valueOf) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+// Archivo: Nodo.java
+import java.util.HashSet;
+import java.util.Set;
+
+public class Nodo {
+    private final char letra;
+    private final int fila;
+    private final int columna;
+    private final Set<Nodo> vecinos; // Nodos adyacentes
+
+    public Nodo(char letra, int fila, int columna) {
+        this.letra = letra;
+        this.fila = fila;
+        this.columna = columna;
+        this.vecinos = new HashSet<>();
     }
-    
+
+    public char getLetra() {
+        return letra;
+    }
+
+    public int getFila() {
+        return fila;
+    }
+
+    public int getColumna() {
+        return columna;
+    }
+
+    public Set<Nodo> getVecinos() {
+        return vecinos;
+    }
+
+    public void agregarVecino(Nodo vecino) {
+        this.vecinos.add(vecino);
+    }
+
+    @Override
+    public String toString() {
+        return "("+fila+","+columna+")[" + letra + "]";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Nodo nodo = (Nodo) o;
+        return fila == nodo.fila && columna == nodo.columna; // Identidad por posición
+    }
+
+    @Override
+    public int hashCode() {
+        // Un hashcode simple pero efectivo para coordenadas 4x4
+        return 31 * fila + columna;
+    }
 }
